4d9c1f79c1582f0b7784be7adcc478c51d654f81
Tim Buckley <timothypbuckley@gmail.com>
2017-07-18 17:02:23 -0400

    Default prefix argument to empty string

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* ACTION TYPER */
 
-module.exports = actionTyper = (prefix) => {
+module.exports = actionTyper = (prefix = '') => {
     return new Proxy({}, {
         get(target, name) {
             return `${prefix}${name}`
